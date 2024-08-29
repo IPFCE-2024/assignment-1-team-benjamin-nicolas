682bf4e9ff25ddfe08f497d1e470ff5822242205
BenjaminKromann <benkromann@gmail.com>
2024-08-29 10:29:33 +0000

    Completed exercise 5

--- src/exercise-5.c
+++ src/exercise-5.c
@@ -2,9 +2,30 @@
 
 int main() {
 
-    // printf("Hello, World!\n");
-    // int input;
-    // scanf("%d", &input);
+    int x;
+    int y;
+    int max; //is going to store the number that is the largest
+
+    printf("Enter the first number \n");
+    scanf("%d", &x);
+    printf("Enter the second number \n");
+    scanf("%d", &y);
+
+    if (x > y)
+    {
+        max = x;
+    }
+    else if (y > x)
+    {
+        max = y;
+    }
+    else if (x == y)
+    {
+        printf("Both numbers are equal");
+        return 0;
+    }
+    
+    printf("The largest number is %d \n", max); 
     
     return 0;
 }
